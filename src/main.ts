e08557ed41ffe1d7f0a331e533551c572b5a20f6
Harvey Rogers <harveyr@gmail.com>
2019-11-11 09:16:23 -0800

    await

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,20 @@ async function run() {
 
   console.log('Diffing against branch %s', baseBranch)
 
-  exec.exec('git', ['diff', '--name-only', '--diff-filter=ACMRT', baseBranch], {
-    listeners: {
-      stdout: (data: Buffer) => {
-        stdout += data.toString()
-      },
-      stderr: (data: Buffer) => {
-        stderr += data.toString()
+  await exec.exec(
+    'git',
+    ['diff', '--name-only', '--diff-filter=ACMRT', baseBranch],
+    {
+      listeners: {
+        stdout: (data: Buffer) => {
+          stdout += data.toString()
+        },
+        stderr: (data: Buffer) => {
+          stderr += data.toString()
+        }
       }
     }
-  })
+  )
 
   console.log('stdout', stdout)
   console.log('stderr', stderr)
