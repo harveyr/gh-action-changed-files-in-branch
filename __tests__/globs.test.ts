9099c6828c31cb21ba2d85f70e0836c3d464b854
Harvey Rogers <harveyr@gmail.com>
2020-01-06 15:44:37 -0800

    more tests cases

--- __tests__/globs.test.ts
+++ __tests__/globs.test.ts
@@ -9,6 +9,9 @@ test.each([
   ['src/some/deep/file.ts', '**/**.ts', true],
   ['src/file.ts', 'src/**.ts', true],
   ['src/file.ts', 'elsewhere/**.ts', false],
+  ['package.json', '*.json', true],
+  ['package.json', 'package.json', true],
+  ['package.json', 'package.*', true],
 ])('glob: %s -> %s == %s', (...args: (string | boolean)[]) => {
   const filename = args[0].toString()
   const glob = args[1].toString()
